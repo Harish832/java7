b32625c4a3a0e901bd5c891e7ac3679d3ea08574
Harish832 <110982422+Harish832@users.noreply.github.com>
2022-10-18 21:14:49 +0530

    Update and rename NewMain7.java to Piglatin.java

--- Piglatin.java
+++ Piglatin.java
@@ -1,19 +1,22 @@
-package newpackage3;
+package newpackage;
 import java.util.Scanner;
-class GFG {
-static boolean isVowel(char c) {
+class PLatin {
+static boolean isVowel(char c)
+{
     return (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
             c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 } 
-static String pigLatin(String s) {
+static String pigLatin(String s) 
+{
     int len = s.length();
     int index = -1;
     for (int i = 0; i < len; i++)
     {
-        if (isVowel(s.charAt(i))) {
+        if (isVowel(s.charAt(i))) 
+        {
         index = i;
         break;
-    }
+        }
     }
     if (index == -1)
         return "-1";
